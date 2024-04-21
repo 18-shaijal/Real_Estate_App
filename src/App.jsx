875f58df18d21113dcf9089ef361bd714df6ed87
18-shaijal <shaijalgupta7@gmail.com>
2024-04-22 01:34:22 +0530

    Up to signin api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './pages/Signin';
+import Signout from './pages/Signout';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import Header from './components/Header';
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>App</h1>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                {/* Uncomment Header if needed */}
+                <Header />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path="/sign-in" element={<Signin />} />
+                    <Route path="/sign-out" element={<Signout />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
